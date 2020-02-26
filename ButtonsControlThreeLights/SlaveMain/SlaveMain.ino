734ab5aa4fb9a635f7768735460bf31875f6ecde
Alexander Nykjær <alax0013@gmail.com>
2020-02-26 07:27:44 +0100

    Update SlaveMain.ino
    Midway update.

--- ButtonsControlThreeLights/SlaveMain/SlaveMain.ino
+++ ButtonsControlThreeLights/SlaveMain/SlaveMain.ino
@@ -1,41 +1,53 @@
-char mystr[5]; //Initialized variable to store recieved data
-int pin1 = 2;
-int pin2 = 3;
-int pin3 = 4;
+const int commandSize = 32;
+const int pin1 = 2;
+const int pin2 = 3;
+const int pin3 = 4;
+char InputOutputString[commandSize]; //Initialized variable to store recieved data
 
 
 void setup() {
-  // Begin the Serial at 9600 Baud
-	Serial.begin(9600);
-	pinMode(pin1,OUTPUT);
-	pinMode(pin2,OUTPUT);
-	pinMode(pin3,OUTPUT);
+  // Begin the Serial1 at 9600 Baud
+  Serial.begin(9600);
+  pinMode(pin1,OUTPUT);
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+}
+
+
+void WriteResponse(char input) {
+  memset(InputOutputString, 0, commandSize);
+  InputOutputString[0] = input;
+  Serial.write(InputOutputString,commandSize);
 }
 
 void loop() {
-	if(Serial.available()) {
-    Serial.readBytes(mystr,5); //Read the serial data and store in var
-    if(mystr[0]=='1'){
-    	if(mystr[1]=='1'){
-    		digitalWrite(pin1, HIGH);
-    	} else{
-    		digitalWrite(pin1, LOW);
-    	}
+  if(Serial.available()) {
+    Serial.readBytes(InputOutputString,commandSize); //Read the Serial1 data and store in var
+    if(InputOutputString[0]=='1'){
+      if(InputOutputString[1]=='1'){
+        digitalWrite(pin1, HIGH);
+      } else{
+        digitalWrite(pin1, LOW);
+      }
+      WriteResponse('1');
+
     }
-    else if(mystr[0]=='2') {
-    	if(mystr[1]=='1'){
-    		digitalWrite(pin2, HIGH);
-    	} else{
-    		digitalWrite(pin2, LOW);
-    	}
-    } else if(mystr[0]=='3') {
-    	if(mystr[1]=='1'){
-    		digitalWrite(pin3, HIGH);
-    	} else{
-    		digitalWrite(pin3, LOW);
-    	}
+    else if(InputOutputString[0]=='2') {
+      if(InputOutputString[1]=='1'){
+        digitalWrite(pin2, HIGH);
+      } else{
+        digitalWrite(pin2, LOW);
+      }
+      WriteResponse('2');
+
+    } else if(InputOutputString[0]=='3') {
+      if(InputOutputString[1]=='1'){
+        digitalWrite(pin3, HIGH);
+      } else{
+        digitalWrite(pin3, LOW);
+      }
+
+      WriteResponse('3');
     }
 }
-    Serial.println(mystr); //Print data on Serial Monitor
-    memset(mystr, 0, sizeof(mystr));
 }
