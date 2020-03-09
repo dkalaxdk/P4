cb35a176a09f164c1bb422295407b3b7e8d2bb09
Alexander Nykjær <alax0013@gmail.com>
2020-03-09 11:52:24 +0100

    Added more base structure.

--- src/sw417f20/ebal/Scanner.java
+++ src/sw417f20/ebal/Scanner.java
@@ -1,4 +1,75 @@
 package sw417f20.ebal;
 
 public class Scanner {
+
+    char currentChar;
+    char nextChar;
+    String whitespace = "\\t\\n\\s";
+
+    public boolean isWhiteSpace() {
+        return whitespace.indexOf(nextChar) != -1;
+    }
+
+
+    public char Peek() {
+        return ' ';
+    }
+
+    public void pop() {
+        // Do code related to popping or finding potential popping errors
+    }
+
+    public Token Advance() {
+
+        if (isWhiteSpace()) {
+            pop();
+            // Check if it is a white space, if white space, advance
+        }
+
+        // Check whether the switch may find another case, ie isDigit, isText or the likes.
+        // Depending on the cases, it should be redirected to something in the likes of "Find keyword" or "Find literal"
+
+        // Set currentChar to nextChar
+        return new Token(Token.Type.EOF,"");
+    }
+
+
+    public char findKeyword(String input) {
+        char output = ' ';
+        Token token = new Token(Token.Type.EOF,"");
+        switch (input) {
+            case "begin":
+                token.type = Token.Type.someKeyword;
+                break;
+            case "master":
+                break;
+            case "slave":
+                break;
+
+
+            default:
+                output = ' ';
+        }
+        return output;
+    }
+
+    public char IsDigit() {
+        return ' ';
+    }
+
+    public void IsChar() {
+
+    }
+
+    public void IsBoolean() {
+
+    }
+
+    public void IsOperator() {
+
+    }
+
+    public void IsIdentifier() {
+
+    }
 }
--- src/sw417f20/ebal/Token.java
+++ src/sw417f20/ebal/Token.java
@@ -1,4 +1,31 @@
 package sw417f20.ebal;
 
 public class Token {
+    public enum Type {
+        //Operators
+        OP_add,OP_subtract,OP_multiply,OP_divide,
+        // Literals
+
+        LIT_Int,LIT_Float,LIT_char,
+
+
+        //Keywords
+        someKeyword,
+
+        //Identifiers
+        Identifier,
+
+
+
+
+        EOF
+
+    }
+    public Type type;
+    public String content;
+
+    public Token(Type inputType,String inputContent) {
+        type = inputType;
+        content = inputContent;
+    }
 }
