012ab0339305ad77cc7867145f17884d95af3c7f
Alexander Nykjær <alax0013@gmail.com>
2020-03-12 09:46:40 +0100

    Added more to switch statement

--- src/sw417f20/ebal/Token.java
+++ src/sw417f20/ebal/Token.java
@@ -2,12 +2,18 @@ package sw417f20.ebal;
 
 public class Token {
     public enum Type {
+        NOTATOKEN,
         //Operators
-        OP_add,OP_subtract,OP_multiply,OP_divide,
+        OP_PLUS, OP_MINUS,OP_EQUALS,OP_TIMES,OP_DIVIDE,OP_NOT,OP_QUESTION,
         // Literals
 
         LIT_Int,LIT_Float,LIT_char,
 
+
+
+        ,COMMA,DOT,BACKSLASH,COLON,SEMI,SINGLEQUOTE,DOUBLEQUOTE
+
+        ,LPAREN,RPAREN,RBRACKET,LBRACKET,RSQBRACKET,LSQBRACKET,
 
         //Keywords
         KEYWORD,
@@ -23,6 +29,8 @@ public class Token {
     }
     public Type type;
     public String content;
+    public int lineNumber;
+    public int offSet;
 
     public Token(Type inputType,String inputContent) {
         type = inputType;
