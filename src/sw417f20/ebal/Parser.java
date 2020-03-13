d0ccf5d7d49e34ee9455a9b4dddac4c4a1dfb76b
Ane Jørgensen <anesoejoe@gmail.com>
2020-03-13 08:48:22 +0100

    Ting

--- src/sw417f20/ebal/Parser.java
+++ src/sw417f20/ebal/Parser.java
@@ -3,6 +3,8 @@ package sw417f20.ebal;
 public class Parser extends RecursiveDescent{
     @Override
     public void Start() {
-//        if (Peek().type == Token.Type.)
+        if (Peek().content == "begin") {
+
+        }
     }
 }
