d54a5c93ccd4010f6b206ff6ff27780f2e765b31
Ane Jørgensen <anesoejoe@gmail.com>
2020-03-16 12:17:09 +0100

    PrintVisitor printer tingene (grimt) ud i rækkefølge

--- src/sw417f20/ebal/PrintVisitor.java
+++ src/sw417f20/ebal/PrintVisitor.java
@@ -2,4 +2,19 @@ package sw417f20.ebal;
 
 public class PrintVisitor extends Visitor {
 
+    @Override
+    public void Visit(Node node) {
+        System.out.println(node.Content);
+
+        if (node.FirstChild != null) {
+            Visit(node.FirstChild);
+
+            Node children = node.FirstChild;
+
+            while (children.Next != null) {
+                Visit(children.Next);
+                children = children.Next;
+            }
+        }
+    }
 }
