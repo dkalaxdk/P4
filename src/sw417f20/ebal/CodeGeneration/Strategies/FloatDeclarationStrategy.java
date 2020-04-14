f5b592c171e61cd94c1def6912870ca63a31fdf4
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2020-04-14 11:45:36 +0200

    Create FloatDeclarationStrategy.java
    Has a condition that checks if there is a value assigned

---
+++ src/sw417f20/ebal/CodeGeneration/Strategies/FloatDeclarationStrategy.java
@@ -0,0 +1,26 @@
+package sw417f20.ebal.CodeGeneration.Strategies;
+
+import sw417f20.ebal.SyntaxAnalysis.AST;
+import sw417f20.ebal.SyntaxAnalysis.Node;
+
+public class FloatDeclarationStrategy extends CodeGenerationStrategy {
+    @Override
+    public String GenerateCode(Node node) {
+        String content = "float ";
+
+        // Add the identifier
+        content += node.FirstChild.Value;
+
+        Node nextChild = node.FirstChild.Next;
+        if(!nextChild.IsEmpty()) {
+            // Assign a value if there is an expression defined.
+            content += " = ";
+            content += nextChild.GenerateCode();
+        }
+        else {
+            content += ";";
+        }
+        content += "\n";
+        return content;
+    }
+}
