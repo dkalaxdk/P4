1bfd72794e136b7d98ffb300a482424fab78a1de
Ane Jørgensen <anesoejoe@gmail.com>
2020-05-12 12:59:36 +0200

    Små ændringer i test fil

---
+++ EBALArduino/CompilerTest/slave_Slave2/slave_Slave2.ino
@@ -0,0 +1,36 @@
+#include <ebal.h> 
+#include <ebalEvent.h> 
+#include <ebalPin.h>
+
+ebalPin out;
+
+intEvent potenTurned;
+
+
+void setup() {
+	out.createPin(analog, output, LED_BUILTIN);
+
+	potenTurned.setID(2);
+
+	Wire.begin(1);
+	Serial.begin(9600);
+	Wire.onReceive(receiveEvent);
+}
+
+void potenTurnedEventHandler0() {
+	out.write(potenTurned.getValue());
+}
+
+
+void receiveEvent(int howMany) {
+	char eventID = Wire.read();
+
+	if (eventID == potenTurned.getID()) {
+		potenTurned.createEvent();
+		potenTurnedEventHandler0();
+	}
+}
+
+void loop() {
+	out.readPin();
+}
